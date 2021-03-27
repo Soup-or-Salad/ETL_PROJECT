5fabfb021e9b4c1410cf5342ff0c07794a958054
Eric Nietzer <75461451+Soup-or-Salad@users.noreply.github.com>
2021-03-26 22:51:14 -0500

    Add files via upload

---
+++ etl_project.sql
@@ -0,0 +1,12 @@
+create database etl_project;
+
+create table Dairy (
+	Produce varchar,
+	Dairy varchar,
+	Grocery varchar,
+	Frozen varchar,
+	Detergents varchar,
+	Deli varchar
+);
+
+Select * FROM Dairy;
